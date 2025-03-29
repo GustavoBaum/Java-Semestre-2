dc89b6dc9bd4112055a2351e646b2f5e8dec9e39
GustavoBaum <163304855+GustavoBaum@users.noreply.github.com>
2025-03-29 09:25:10 -0300

    Add files via upload

---
+++ Lista de Exercicios 06/ListaExercicio6/src/Questao1/Funcionario.java
@@ -0,0 +1,51 @@
+package Questao1;
+
+public class Funcionario {
+
+    private  String nome;
+    private double salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalario(double salario) throws IllegalArgumentException {
+        if (salario < 0) {
+            throw new IllegalArgumentException("Salário deve ser maior que Zero");
+        }
+        this.salario = salario;
+    }
+
+    public double calcularIrpf(double salario) {
+        if (salario <= 1903.98) {
+            System.out.print("Isento de IRPF");
+        }
+        if (salario > 1903.98 && salario <= 2826.65) {
+            return salario = (2826.65 - 1903.98) * 0.075;
+        }
+        if (salario > 2826.65 && salario <= 3751.05) {
+            return salario = ((salario - 2826.65) * 0.15) + (2826.65 - 1903.98) * 0.075;
+        }
+        if (salario > 3751.05 && salario <= 4664.68) {
+            return salario = (4664.68 - 3751.05) * 0.225 + (((salario - 2826.65) * 0.15) + (2826.65 - 1903.98) * 0.075);
+        }
+        if (salario > 4664.68) {
+            return salario = (salario - 4664.68) * 0.275 + (4664.68 - 3751.05) * 0.225 + ((salario - 2826.65) * 0.15) + (2826.65 - 1903.98) * 0.075;
+        }
+        return this.salario;
+    }
+
+    public enum identificarFaixaIrpf{
+        PRIMEIRA, SEGUNDA, TERCEIRA, QUARTA, QUINTA
+    }
+    
+}
+
